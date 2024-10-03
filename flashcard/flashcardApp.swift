bdd33a3567ac958f811f59b11666dd48e684a0b4
meghan t <meghanthomas2022@fau.edu>
2024-10-03 18:41:33 -0400

    initial commit

---
+++ flashcard/flashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashcardApp.swift
+//  flashcard
+//
+//  Created by meghan on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct flashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
