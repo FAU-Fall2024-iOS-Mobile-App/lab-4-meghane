0185764b55f04426af5a6b3948b27ee1c7f892cb
meghan t <meghanthomas2022@fau.edu>
2024-10-03 19:42:23 -0400

    created the flashcard UI

--- flashcard/ContentView.swift
+++ flashcard/ContentView.swift
@@ -9,16 +9,28 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            
+            // Card background
+            RoundedRectangle(cornerRadius: 25.0)
+                .fill(Color.cyan.gradient)
+                .shadow(color: .black, radius: 4, x: -2, y: 2)
+            VStack(spacing: 20) {
+                // Card text
+                Text("Question")
+                        .bold()
+                // Separator
+                Rectangle()
+                    .frame(height: 1)
+                Text("Located at the southern end of Puget Sound, what is the capitol of Washington?")
+            }
+            .font(.title)
+            .foregroundStyle(.white)
+            .padding(30)
         }
-        .padding()
+        .frame(width: 300, height: 500)
     }
 }
-
 #Preview {
     ContentView()
 }
