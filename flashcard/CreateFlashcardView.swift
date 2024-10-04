acb05d73ae4b4cbbec21b99753c1aa6a2bf4f408
meghan t <meghanthomas2022@fau.edu>
2024-10-03 21:52:02 -0400

    created a view to add a flashcard

---
+++ flashcard/CreateFlashcardView.swift
@@ -0,0 +1,18 @@
+//
+//  CreateFlashcardView.swift
+//  flashcard
+//
+//  Created by meghan on 10/3/24.
+//
+
+import SwiftUI
+
+struct CreateFlashcardView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CreateFlashcardView()
+}
